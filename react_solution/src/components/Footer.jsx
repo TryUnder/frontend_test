5f63d58199667b4a59781be34dff85e852e0a69a
TryUnder <sniacyinaczej@gmail.com>
2024-07-11 19:48:41 +0200

    update

--- react_solution/src/components/Footer.jsx
+++ react_solution/src/components/Footer.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import '../scss/Footer.scss';
 
 const Footer = ({FontAwesomeIcon, faChevronUp}) => {
+    const [ isPanelVisible, setPanelVisible ] = useState(false)
+
+    const togglePanel = () => {
+        setPanelVisible(!isPanelVisible)
+    }
+
+    const resetContent = () => {
+        localStorage.removeItem('content');
+        window.location.reload();
+    }
+
+    const appendName = () => {
+        document.getElementById('user-name').innerHTML += '<br>Hubert Lewowicki';
+    }
+
     return (
         <>
             <footer className="footer">
@@ -9,12 +24,20 @@ const Footer = ({FontAwesomeIcon, faChevronUp}) => {
                     <p className="footer-text">CSS<br/> IS<br/> AWESOME<br/></p>
                     <p className="footer-text-logo">nabthat</p>
                     <div>
-                        <button className="footer-button">
+                        <button className="footer-button" onClick = {togglePanel}>
                             POKAŻ
                             <FontAwesomeIcon icon={faChevronUp} />
                         </button>
                     </div>
                 </div>
+                {
+                    isPanelVisible && (
+                        <div className="footer-panel">
+                            <button onClick={resetContent}>ZRESETUJ USTAWIENIA</button>
+                            <button onClick={appendName}>POKAŻ DANE OSOBOWE</button>
+                        </div>
+                    )
+                }
             </footer>
         </>
     )
--- react_solution/src/components/Header.jsx
+++ react_solution/src/components/Header.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import '../scss/Header.scss';
-import logo from '../images/logo.png';
+import logoGrey from '../images/logo_gray.png';
 
 const Header = () => {
     return (
         <>
             <header>
                 <a href='/'>
-                    <img src={logo} alt="Logo" className="logo"/>
+                    <img src={logoGrey} alt="Logo" className="logo"/>
                 </a>
-                <p className="header-par">Zadanie <b>rekrutacyjne</b></p>
+                <p className="header-par" id="user-name">Zadanie <b>rekrutacyjne</b></p>
             </header>
         </>
     )
